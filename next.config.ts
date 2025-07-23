4a92a2ee37fb5ece8936732cac761d69847c69b2
bilgehan.aygun <bilgehan.aygun@tubitak.gov.tr>
2025-07-23 11:28:18 +0300

    pino-pretty disable

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "pino-pretty": false,
+      };
+    }
+    return config;
+  },
+  output: "standalone",
 };
 
 export default nextConfig;
